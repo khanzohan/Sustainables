35e133d2f670d86eb0aaf8669adc7ee01ecadee7
khanzohan <zoho12@live.com>
2019-11-30 12:32:14 -0500

    Added routing with react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 
-function App() {
+const Hats = () => {
   return (
-    <HomePage />
+    <div>
+    <h1>Hats</h1>
+  </div>
+  );
+};
+
+const Balenciaga = (props) => {
+  return (
+    <div>
+      <h1>x1's</h1> 
+      <button onClick={() => props.history.push('/hats')}>Hats</button>
+      <Link to={`${props.match.url}/15`}>Versace</Link>
+      <Link to={`${props.match.url}/16`}>Chanel</Link>
+    </div>
+  )
+}
+
+const App = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <Route path='/' component={HomePage} />
+      <Route path='/hats' component={Hats} />
+      <Route path='/hats/:hatId' component={Balenciaga} />
+    </div>
   );
 }
 
